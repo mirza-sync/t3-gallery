15ad94f8b20c916ab0082d4582e3b18139e5735d
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-03 15:08:50 +0800

    Display image info

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Reference: [From 0 to Production - The Modern React Tutorial (RSCs, Next.js, Sha
 - [x] "taint" (server-only). [Explanation](https://www.youtube.com/watch?v=JrKBinrMGR4)
 - [x] Use Next/Image component
 - [x] Error management (w/ Sentry)
-- [] Routing/image page (parallel route)
+- [x] Routing/image page (parallel route)
 - [] Update upload button to be less cringe
 - [] Analytics (posthog)
 - [] Delete button (w/ Server Actions)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,9 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
     const image = await getImage(props.id)
+    const uploaderInfo = await clerkClient.users.getUser(image.userId)
 
     return (
         <div className="flex w-full h-full">
@@ -9,7 +11,15 @@ export default async function FullPageImageView(props: { id: number }) {
                 <img src={image.url} className="object-contain" />
             </div>
             <div className="w-48 flex flex-col flex-shrink-0 border-l">
-                <div className="text-xl font-semibold">{image.name}</div>
+                <div className="text-lg text-center border-b p-2">{image.name}</div>
+                <div className="flex flex-col p-2">
+                    <span>Uploaded By:</span>
+                    <span>{uploaderInfo.fullName}</span>
+                </div>
+                <div className="flex flex-col p-2">
+                    <span>Created On:</span>
+                    <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+                </div>
             </div>
         </div>
     );
