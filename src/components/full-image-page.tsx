5c1c6a182336e1b9f38c141ad302f3d49e4088a6
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-03 14:00:26 +0800

    Add styling to image view

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -4,6 +4,13 @@ export default async function FullPageImageView(props: { id: number }) {
     const image = await getImage(props.id)
 
     return (
-        <img src={image.url} className="w-96" />
+        <div className="flex w-full h-full">
+            <div className="flex-shrink my-auto">
+                <img src={image.url} className="object-contain" />
+            </div>
+            <div className="w-48 flex flex-col flex-shrink-0 border-l">
+                <div className="text-xl font-semibold">{image.name}</div>
+            </div>
+        </div>
     );
 }
