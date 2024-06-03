ddbb3e3a8a58d3c97d94392d37f8fbd1b980de49
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-03 12:33:26 +0800

    Fetch and display one image

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,14 @@
-export default function PhotoModal({
-  params: { id: photoId },
+import { getImage } from "~/server/queries";
+
+export default async function PhotoModal({
+  params: { id },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = Number(id)
+  if (isNaN(idAsNumber)) throw new Error("Invalid image id")
+
+  const image = await getImage(idAsNumber)
+
+  return <div><img src={image.url} className="w-96" /></div>;
 }
