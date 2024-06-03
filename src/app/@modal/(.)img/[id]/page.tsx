688ad9214350cc38aad9bbac9cf3f627a137fab4
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-03 13:01:01 +0800

    Add image modal

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,6 +1,7 @@
-import { getImage } from "~/server/queries";
+import { Modal } from "./modal";
+import FullPageImageView from "~/components/full-image-page";
 
-export default async function PhotoModal({
+export default function PhotoModal({
   params: { id },
 }: {
   params: { id: string };
@@ -8,7 +9,9 @@ export default async function PhotoModal({
   const idAsNumber = Number(id)
   if (isNaN(idAsNumber)) throw new Error("Invalid image id")
 
-  const image = await getImage(idAsNumber)
-
-  return <div><img src={image.url} className="w-96" /></div>;
+  return (
+    <Modal>
+      <FullPageImageView id={idAsNumber} />
+    </Modal>
+  );
 }
