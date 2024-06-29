6695ff04c47142ffcfafa4e8d7f4cd5e372b88ef
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-29 17:53:12 +0800

    Remove comments

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -6,23 +6,16 @@ import { images } from "~/server/db/schema";
  
 const f = createUploadthing();
  
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 40 } })
-    // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
       const user = auth();
  
-      // If you throw, the user will not be able to upload
       if (!user.userId) throw new UploadThingError("Unauthorized");
  
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
  
       await db.insert(images).values({
