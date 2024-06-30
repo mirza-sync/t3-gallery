ec2ff7813aeb0864a5f7f943ea8b5adf86b84507
mirza-sync <mirzasyahmi123@gmail.com>
2024-06-30 22:36:29 +0800

    Adjust ratelimit

--- src/server/ratelimit.ts
+++ src/server/ratelimit.ts
@@ -1,10 +1,10 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-// Create a new ratelimiter, that allows 10 requests per 10 seconds
+// Create a new ratelimiter, that allows 10 requests per 100 seconds
 export const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(2, "100 s"),
+  limiter: Ratelimit.slidingWindow(10, "100 s"),
   analytics: true,
   /**
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
